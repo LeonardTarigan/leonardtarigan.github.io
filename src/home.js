a0221b7b21cb07932cdcd552ea36a6951b545e81
LeonardTarigan <leontrg32@gmail.com>
2022-04-05 19:08:28 +0700

    added typed js for home page

--- src/home.js
+++ src/home.js
@@ -1,16 +1,12 @@
-// const text = document.getElementById("greeting");
-const text = $("#greeting");
-let words = ["こんにちは!", "Bonjour!", "Hola!", "Ciao!", "你 好!", "Hello World!"];
-let i = 0;
+var typed = new Typed('.hello', {
+    strings: ["Hello World!", "こんにちは!", "Bonjour!", "Hola!", "Ciao!", "你 好!"],
+    typeSpeed: 150,
+    backSpeed: 70,
+    backDelay: 2000,
+    loop: true,
+});
 
-setInterval(function () {
-    text.fadeOut(function () {
-        text.html(words[i]);
-        i++;
-        if (i >= words.length) {
-            i = 0;
-        }
-        text.fadeIn();
-    });
-}, 3000);
+
+
+
 
