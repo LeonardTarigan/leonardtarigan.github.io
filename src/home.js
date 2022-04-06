673799382a6b832ef84c147ad15bb85774916647
LeonardTarigan <leontrg32@gmail.com>
2022-04-06 23:12:15 +0700

    changed image

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 var typed = new Typed('.hello', {
     strings: ["Hello World!", "こんにちは!", "Bonjour!", "Hola!", "Ciao!", "你 好!"],
     typeSpeed: 150,
-    backSpeed: 70,
+    backSpeed: 90,
     backDelay: 2000,
     loop: true,
 });
